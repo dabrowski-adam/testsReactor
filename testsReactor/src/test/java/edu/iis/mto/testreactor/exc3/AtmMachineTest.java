54fe8823d1baae2ae6241263a2298b9b30d14484
Adam Dąbrowski <adam.dabrowski@htdevelopers.com>
2019-05-16 16:39:01 +0200

    Check if withdraw throws on zero money

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -2,14 +2,36 @@ package edu.iis.mto.testreactor.exc3;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class AtmMachineTest {
+
+    private CardProviderService cardService;
+    private BankService bankService;
+    private MoneyDepot moneyDepot;
+
+    @Before
+    public void setUp() {
+        cardService = mock(CardProviderService.class);
+        bankService = mock(BankService.class);
+        moneyDepot = mock(MoneyDepot.class);
+    }
 
     @Test
     public void itCompiles() {
         assertThat(true, equalTo(true));
     }
 
+    @Test(expected = WrongMoneyAmountException.class)
+    public void withdraw_zero_throws() {
+        AtmMachine atmMachine = new AtmMachine(cardService, bankService, moneyDepot);
+
+        Money money = Money.builder().withAmount(0).withCurrency(Currency.PL).build();
+        Card card = Card.builder().withCardNumber("").withPinNumber(123).build();
+
+        atmMachine.withdraw(money, card);
+    }
 }
